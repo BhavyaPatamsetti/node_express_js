ed19dbf8b59f5f80d7e0c6c23facd34d525d40e6
BhavyaPatamsetti <86647036+BhavyaPatamsetti@users.noreply.github.com>
2022-06-29 14:10:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,63 +1,34 @@
-console.log("Example node js application"); //you can also omit semicolon
-console.log(2+6)
-console.log("2"+8)
-let a=10;
-let b=10;
-console.log(a+b);
-let x=10, y= 20;
-console.log(x+y);
-let c=a+b;
-console.log(c);
+// Include ./utils/geoloc
+// Include ./utils/weatherinfo
 
-// given a word convert that to sorted alphabetical order
-function alpha_sort(str) { 
-    return str.split("").sort().join("");       
-}
+var geoloc = require('./utils/geoloc')
+var weatherinfo = require('./utils/weatherinfo')
 
-console.log("session".split());
-console.log("ses sion".split(" "));
-
-//callback functions
-function verifylogin(user, pwd, callback){
-    if(pwd == "pwd1"){
-        callback(null, "correct login")
-    }else{
-        callback("Incorrect login", null)
+geoloc.geoloc("Bhimavaram",function(error,locjson){
+    if(error){
+        console.log(error)
     }
-}
-
-verifylogin("user1","pwd1",function(err, result){
-    if(err){
-        console.log("invalid");
-    }else{
-        console.log("valid user");
-    }
-})
-// 2nd check
-verifylogin("user1","pwd2",function(err, result){
-    if(err){
-        console.log("invalid");
-    }else{
-        console.log("valid user");
+    else{
+        console.log("Bhimavaram Longitude:"+locjson.longitude+" Latitude:"+locjson.latitude)
+        
     }
 })
 
-//from 10th version to 16 th version callback function got updated
-verifylogin("user1","pwd2",(err, result)=>{
-    if(err){
-        console.log("invalid");
-    }else{
-        console.log("valid user");
-    }
-})
+weatherinfo.getWeatherInfo("Bhimavaram",function(error,temperatureinfo){
+            if(error){
+                console.log(error)
+            }
+            else{
+                console.log("Current Temperature in Bhimavaram:"+temperatureinfo)
+            }
+        
+        })
 
-console.log("program is starting");
 
-setTimeout(()=> {
-    console.log("Inside timeout");
-}, 2000);
+//Test the following Case and observe the output.
+// CASE 1:Copy and Paste the above code to test with another valid city and observe the output.
 
-console.log("after time out");
-console.log("program ending");
+// CASE 2:Copy and Paste the above code to test with invalid city value and observe the output.
 
-//only fs, os, console are the only 3 modules
+
+
